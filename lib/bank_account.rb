a1269b7bc18b4541b5b242f2d7991349d4cac2a7
Junaid Malik <jmalik981@gmail.com>
2020-01-25 22:03:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,13 @@
 class BankAccount
 attr_reader :name
 attr_accessor :balance, :status
+
+def initalize(name)
+  @name= name
+  @balance = 1000
+  @status = "open"
+end
+
+def deposit(deposit_amount)
+  self
 end
