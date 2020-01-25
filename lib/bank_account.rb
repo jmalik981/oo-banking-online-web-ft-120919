d5375814c4d044cb3d1eb82b49a589dda3267b92
Junaid Malik <jmalik981@gmail.com>
2020-01-25 22:05:08 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,5 +9,16 @@ def initalize(name)
 end
 
 def deposit(deposit_amount)
-  self
+  self.balance += deposit_amount
+end
+def display_balance
+  "your balance is $#{balance}."
+end
+def close_account
+  self.status = "closed"
+end
+def valid?
+  balance > 0 && status == "open"
+end
+
 end
